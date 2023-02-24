6c92ac6348f4a434941acfa4ade64663b77b3fd8
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-02-24 22:57:55 +0600

    Update 01-gallery.js
    rendering galleryItems = [{}]

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMurkUp = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMurkUp);
+console.log(createGallery(galleryItems));
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class='gallery__item'><a class='gallery__link'  href="${original}"><img class='gallery__image' src="${preview}" data-source="${original}" alt="${description}"></img></a></div>`;
+    })
+    .join('');
+  return murkup;
+}
